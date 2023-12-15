ed6ad249e160e8fa1b07623de4688aececec109b
NyctibiusVII <matheus.dev.07@gmail.com>
2023-12-15 09:52:10 -0300

    feat: Creating all the necessary stylization

--- src/script.js
+++ src/script.js
@@ -1,7 +1,12 @@
 function toggleLogin() {
     const toggleLoginType = document.getElementById('toggle-login-type')
+    const iconToggleLoginType = document.getElementById('arrow-icon')
+    const logoIcon = document.getElementById('logo-icon')
+
     const sectionLoginDefault = document.getElementById('section-login-default')
     const sectionLoginVoucher = document.getElementById('section-login-voucher')
+
+    logoIcon.classList.toggle('slide-left')
 
     toggleLoginType.className =
         toggleLoginType.className === 'type-login-voucher'
@@ -10,16 +15,18 @@ function toggleLogin() {
 
     if(toggleLoginType.className === 'type-login-voucher') {
         toggleLoginType.textContent = 'Área de colaboradores'
+        toggleLoginType.appendChild(iconToggleLoginType)
 
         sectionLoginDefault.style.height = '0'
-        sectionLoginVoucher.style.height = '100%'
+        sectionLoginVoucher.style.height = '10.625em'
 
         return
     }
 
     toggleLoginType.textContent = 'Área de visitantes'
+    toggleLoginType.appendChild(iconToggleLoginType)
 
-    sectionLoginDefault.style.height = '100%'
+    sectionLoginDefault.style.height = '16.563em'
     sectionLoginVoucher.style.height = '0'
 }
 
@@ -36,4 +43,4 @@ function togglePasswordVisibility() {
     }
 
     passwordInput.type = 'password'
-  }
+}
